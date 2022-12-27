3de4565a1233aeaa6477959b68231f84ae818253
17839307 <GavryushKin.S.I.@sberbank.ru>
2022-12-27 15:31:21 +0300

    First commit

---
+++ src/test/java/ru/app/draft/DraftApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.app.draft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
